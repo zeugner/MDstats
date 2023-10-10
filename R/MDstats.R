81626949215d6d9f92c2690edd36280617b41b67
ZEUGNER Stefan (ECFIN) <Stefan.ZEUGNER@ec.europa.eu>
2023-10-10 23:36:53 +0200

    another fix regarding onLoad

--- R/MDstats.R
+++ R/MDstats.R
@@ -29,7 +29,6 @@ NULL
 .onLoad = function (libname, pkgname) {
   require(data.table); require(XML); require(MD3)
   #message(libname,pkgname)
-<<<<<<< HEAD
 
   #warning('provtbl ',exists('providertable', envir = topenv()))
  # utils::data('providertable', envir=topenv())
@@ -37,18 +36,6 @@ NULL
 
   # assign('.mdstats_providers', .mdstats_providerscreate(providertable), envir = topenv())
   # .rsdmxfixer()
-=======
-#     providertablepath=dir(paste0(libname,'/',pkgname),pattern = 'providers.csv',recursive = TRUE,full.names = TRUE)[1L]
-#     providertable=utils::read.csv(providertablepath,stringsAsFactors = FALSE,header = TRUE, na.strings='')
-#     rownames(providertable) = providertable[[1]]
-#     colnames(providertable)[[1]]='AgencyID'; rownames(providertable)=providertable[[1]]
-#     assign('providertable', providertable, envir = topenv())
-  utils::data('providertable', envir=topenv())
-  #assign('providertable', , envir = topenv())
-    .rsdmxfixer()
-  assign('.mdstats_providers', .mdstats_providerscreate(providertable), envir = topenv())
-
->>>>>>> a97d136877c512af65b478b93f510929503f6eb2
 
 }
 
